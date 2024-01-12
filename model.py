89b8e8c3e7f29888593364c7bcf8171732ce0ae5
jarredon <jarredon@student.42malaga.com>
2024-01-12 20:03:14 +0100

    loss function

--- model.py
+++ model.py
@@ -15,49 +15,61 @@ Y = data.iloc[:, 1]
 
 # Building the model
 m = 0
-c = 0
+c = 9000
 
-L = 0.000000000001  # The learning Rate
-epochs = 200  # The number of iterations to perform gradient descent
+L = 0.00000000001  # The learning Rate
+epochs = 100  # The number of iterations to perform gradient descent
 
 n = float(len(X)) # Number of elements in X
 
+emes = [0]
+ces = [9000]
+
 # Performing Gradient Descent 
-# for i in range(epochs): 
-#     Y_pred = m*X + c  # The current predicted value of Y
-#     D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
-#     D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
-#     m = m - L * D_m  # Update m
-#     c = c - L * D_c  # Update c
-#     loss = (-1/n) * (sum(Y - Y_pred))**2
-#     # print (m, c)
-#     print(loss)
+for i in range(epochs): 
+    Y_pred = m*X + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c
+    loss = (-1/n) * (sum(Y - Y_pred))**2
+    # print (m, c)
+    print(f'm: {m}, c: {c}, loss: {loss}')
+    emes.append(m)
+    ces.append(c)
     
-# print (m, c)
+print (m, c)
 
 def loss_fun():
-    mp = np.outer(np.linspace(-1000000000, 1000000000, int(n)), np.ones(int(n)))
-    cp = mp.copy().T
-    xx = np.outer(X, np.ones(int(n)))
-    yy = np.outer(Y, np.ones(int(n)))
-    y_pre = mp*xx + cp
-    zp = (-1/n) * (sum(yy - y_pre))**2
+    def error(m, c):
+        totalError = 0
+        for i in range(0, int(n)):
+            totalError += (Y[i] - (m * X[i] + c)) ** 2
+        return totalError / n
 
-    print(cp.shape)
-    print(yy.shape)
-    print(y_pre.shape)
-    print(zp.shape)
+    mp, cp = np.meshgrid(np.linspace(-1000, 1000, int(n)), np.linspace(-1000, 1000, int(n)))
+    # xx = np.outer(X, np.ones(int(n)))
+    # yy = np.outer(Y, np.ones(int(n)))
+    # y_pre = mp*X + cp
+    # zp = (-1/n) * (sum(Y - y_pre))**2
+    zp = np.array([error(m, c) for m, c in zip(np.ravel(mp), np.ravel(cp))])
+    zp = zp.reshape(mp.shape)
 
     ax = plt.axes(projection='3d')
-    ax.plot_surface(mp, cp, zp, cmap='viridis', edgecolor='green')
+    ax.plot_surface(mp, cp, zp, cmap='viridis', edgecolor='green', alpha=0.5)
     # ax.plot3D(mp, cp, zp, 'green')
+    ax.set_xlabel('m')
+    ax.set_ylabel('b')
+    ax.set_zlabel('error')
+    zetas = [error(m, c) for m, c in zip(emes, ces)]
+    ax.plot(emes, ces, zetas, color='red', linewidth=2)
     plt.show()
 
 loss_fun()
 
 # Making predictions
-# Y_pred = m*X + c
+Y_pred = m*X + c
 
-# plt.scatter(X, Y) 
-# plt.plot(X, Y_pred, color='red')  # regression line
-# plt.show()
+plt.scatter(X, Y) 
+plt.plot(X, Y_pred, color='red')  # regression line
+plt.show()
